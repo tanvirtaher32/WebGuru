ccb9c3d284d0f74477c0bb9a8452f7eae18394cb
tanvirtaher32 <tanvirtaher7@gmail.com>
2023-10-06 18:33:52 +0600

    Home section coding 50% done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import vg from '../../assets/2.webp';
 
 const Home = () => {
     return (
-        <div>
-            <h2>Home</h2>
+        <>
+        
+        <div className='home'>
+            <main>
+                <h1>
+                    Web Guru
+                </h1>
+                <p>Solution to all your problems</p>
+            </main>
         </div>
+
+        <div className="home2">
+            <img src={vg} alt="" />
+
+            <p>
+            Welcome to Web Guru, your trusted partner in digital solutions. Explore a world of innovation where we harness technology to empower your business, from cutting-edge software development to strategic digital consultancy. Discover how we can elevate your digital presence and drive growth in the digital age. 
+            </p>
+        </div>
+        
+        </>
     );
 };
 
